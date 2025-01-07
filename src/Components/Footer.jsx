3c394c1409f12a42f7a7211cd5ce56b4e7fb85ef
krishbhutani <krishbhutani7417@gmail.com>
2025-01-07 16:58:51 +0530

    ecell

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,10 +3,10 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
     return (
-        <div className='bg-[#F3F2E3] hello px-28 py-16'>
-            <div className='flex'>
+        <div className='bg-[#F3F2E3] sm:px-28 px-8 sm:py-16 py-10'>
+            <div className='flex sm:flex-row flex-col'>
                 <div>
-                    <img className="w-5/6" src={assets.Footer_logo} alt="" />
+                    <img className="w-full" src={assets.Footer_logo} alt="" />
 
                     <div className='flex items-center gap-2 sm:gap-4'>
                         <img className='h-12 w-auto'  src={assets.linkedin_logo} alt="" />
@@ -15,29 +15,29 @@ const Footer = () => {
                         <img className='h-12 w-auto'  src={assets.yt_logo} alt="" />
                     </div>
                 </div>
-                <div className='flex pl-72 gap-14'>
+                <div className='flex flex-row  sm:pl-72 pl-0 sm:gap-14 gap-7 sm:mt-0 mt-5 '>
                     <div>
-                        <div className='font-medium text-xl mb-4'>Links</div>
+                        <div className='font-medium sm:text-xl text-lg sm:mb-4 mb-2'>Links</div>
 
-                        <ul className='flex flex-col gap-2'>
-                            <a className='text-xl' href="#">HOME</a>
-                            <a className='text-xl' target="_blank" href="https://nith.ac.in/">NITH</a>
-                            <a className='text-xl' href="#">About Us</a>
-                            <a className='text-xl' href="#Team">Our Team</a>
-                            <a className='text-xl' href="#">FAQs</a>
+                        <ul className='flex flex-col sm:gap-2 gap-0'>
+                            <a className='sm:text-xl text-lg' href="#">HOME</a>
+                            <a className='sm:text-xl text-lg' target="_blank" href="https://nith.ac.in/">NITH</a>
+                            <a className='sm:text-xl text-lg' href="#">About Us</a>
+                            <a className='sm:text-xl text-lg' href="#Team">Our Team</a>
+                            <a className='sm:text-xl text-lg' href="#">FAQs</a>
 
                         </ul>
 
                     </div>
 
                     <div>
-                        <div className='font-medium text-xl mb-4'>Our Initiatives</div>
+                        <div className='font-medium sm:text-xl text-lg sm:mb-4 mb-2'>Our Initiatives</div>
 
-                        <ul className='flex flex-col gap-2'>
-                            <a className='text-xl' href="#">E-Summit</a>
-                            <a className='text-xl' href="#">SIP</a>
-                            <a className='text-xl' href="#">HULT Prize</a>
-                            <a className='text-xl' href="https://e-crescendo.vercel.app">E-Crescendo</a>
+                        <ul className='flex flex-col sm:gap-2 gap-0'>
+                            <a className='sm:text-xl text-lg' href="#">E-Summit</a>
+                            <a className='sm:text-xl text-lg' href="#">SIP</a>
+                            <a className='sm:text-xl text-lg' href="#">HULT Prize</a>
+                            <a className='sm:text-xl text-lg' href="https://e-crescendo.vercel.app">E-Crescendo</a>
 
                         </ul>
 
@@ -45,10 +45,12 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className='mt-5'>
-                <span className='sm:text-9xl text-5xl font-extrabold text-[#4BA3E9]'>Entrepreneurship</span>
-                <span className='sm:text-9xl text-5xl font-extrabold ml-10 text-[#4BA3E9]' >Cell</span>
-            </div>
+           
+                <div className='mt-5 sm:text-8xl text-xl font-extrabold text-[#4BA3E9]'>
+                Entrepreneurship Cell
+                </div>
+                
+        
         </div>
     )
 }
--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -4,15 +4,15 @@ import { assets } from '../assets/assets'
 const Gallery = () => {
     return (
         <div>
-            <div className='w-full  pt-10 px-36'>
-                <div className='text-4xl font-medium flex items-center gap-3 '>
+            <div className='w-full  sm:pt-10 pt-5 sm:px-36 px-7'>
+                <div className='sm:text-4xl text-3xl font-medium flex items-center gap-3  '>
                     <span className='text-slate-500'>Our</span><span>Gallery</span>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-20">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
                     </svg>
 
                 </div>
-                <div className='w-5/12 font-medium font-title text-slate-500'>
+                <div className='sm:w-5/12 w-full font-medium font-title text-slate-500'>
                     Step into the vibrant world of E-Cell NIT Hamirpur through our gallery, capturing moments of inspiration, collaboration, and entrepreneurial zeal from our events and initiatives.
                 </div>
 
