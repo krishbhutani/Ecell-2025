44f43f59211af84d38e8570a992652aebe592ed2
krishbhutani <krishbhutani7417@gmail.com>
2025-01-22 16:36:43 +0530

    Prcahi

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -55,7 +55,7 @@ import About1 from "./About1.jpg"
 import Rishikesh_Mishra from "./Rishikesh_Mishra.jpeg"
 import Aryan_mittal from "./Aryan_mittal.jpeg"
 import jp from "./JP.png"
-import Prachi from "./prachi.jpg"
+import Prachi from "./Prachi.jpg"
 
 import Yash_Panthri_alumini from "./Yash_Panthri_alumini.png"
 import Sagar_Attri_Alumini from "./Sagar_Attri_Alumini.jpeg"
